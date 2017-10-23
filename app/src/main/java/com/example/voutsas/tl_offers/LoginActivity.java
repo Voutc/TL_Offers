ba031ffdc0f5e9396d667827ab997aaf874deaca
Voutsas <voutsf@gmail.com>
2017-10-23 22:19:03 +0300

    Make the Intent for Register Activity

--- app/src/main/java/com/example/voutsas/tl_offers/LoginActivity.java
+++ app/src/main/java/com/example/voutsas/tl_offers/LoginActivity.java
@@ -1,7 +1,12 @@
 package com.example.voutsas.tl_offers;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -9,5 +14,19 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        final EditText etUsername = (EditText) findViewById(R.id.etUsername);
+        final EditText etPassword = (EditText) findViewById(R.id.etPassword);
+        final Button btnLogin = (Button) findViewById(R.id.btnLogin);
+        final TextView regLink = (TextView) findViewById(R.id.regLink);
+
+        regLink.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent regIntent = new Intent(LoginActivity.this,Register.class);
+                LoginActivity.this.startActivity(regIntent);
+            }
+        });
+
+
     }
 }
