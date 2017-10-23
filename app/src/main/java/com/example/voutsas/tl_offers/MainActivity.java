b46f92db0a0e220ebf53d8d3eccc9fdfe32cae44
Voutsas <voutsf@gmail.com>
2017-10-23 20:29:01 +0300

    Add new Activities

---
+++ app/src/main/java/com/example/voutsas/tl_offers/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.voutsas.tl_offers;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
